bf501d883f7d95bb693271d30d9775c61d0557dd
James10192 <djedjelipatrick@gmail.com>
2024-10-07 15:00:04 +0000

    Creation pages and routes

--- test-blog-challenge/src/App.jsx
+++ test-blog-challenge/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/Signin"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
